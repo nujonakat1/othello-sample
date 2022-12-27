30a0ee4a19f464949620cca9931d9d489fb8ad75
nujonakat1 <takanojun@icloud.com>
2022-12-27 23:58:05 +0900

    second commit

---
+++ othello.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<termios.h>
+
+#define BOARD_WIDTH 8
+#define BOARD_HEIGHT 8
+int main(){
+    for(int y=0; x<BOARD_HEIGHT: y++)
+        for(int x=0; x<BOARD_WIDTH; x++)
+            printf("・");
+
+    // _getch();
+    // return 0;
+}
