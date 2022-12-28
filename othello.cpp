cdaceec0c9c6fdfd37a115519ebcb321665a7813
nujonakat1 <takanojun@icloud.com>
2022-12-28 22:35:21 +0900

    third commit

--- othello.cpp
+++ othello.cpp
@@ -1,13 +1,30 @@
 #include<stdio.h>
+#include<wchar.h>
+#include<iostream>
 #include<termios.h>
 
 #define BOARD_WIDTH 8
 #define BOARD_HEIGHT 8
-int main(){
-    for(int y=0; x<BOARD_HEIGHT: y++)
-        for(int x=0; x<BOARD_WIDTH; x++)
-            printf("・");
 
-    // _getch();
-    // return 0;
+int cursorX, cursorY;
+
+int main(){
+    switch(getchar()){
+    case'w': cursorY--; break;
+    case's': cursorY++; break;
+    case'a': cursorX--; break;
+    case'd': cursorX++; break;
+    default:
+        break;
+    }
+    for(int y=0; y<BOARD_HEIGHT; y++) {
+        for(int x=0; x<BOARD_WIDTH; x++)
+            if((x==cursorX) && (y==cursorY))
+                printf("◎");
+            else    
+                printf("・");
+        printf("\n");
+    }
+   
+
 }
