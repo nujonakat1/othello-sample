a35ad35b97b6d8820ac280fef3467708034ae3f9
nujonakat1 <takanojun@icloud.com>
2022-12-29 10:04:42 +0900

    fourth commit

--- othello.cpp
+++ othello.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<wchar.h>
 #include<iostream>
 #include<termios.h>
@@ -6,25 +7,41 @@
 #define BOARD_WIDTH 8
 #define BOARD_HEIGHT 8
 
+enum{
+    COLOR_NOME=-1,
+    COLOR_BLACK=0,
+    COLOR_WHITE=1,
+    COLOR_MAX
+};
+
+int cells[BOARD_WIDTH][BOARD_HEIGHT];
 int cursorX, cursorY;
 
 int main(){
-    switch(getchar()){
-    case'w': cursorY--; break;
-    case's': cursorY++; break;
-    case'a': cursorX--; break;
-    case'd': cursorX++; break;
-    default:
-        break;
-    }
-    for(int y=0; y<BOARD_HEIGHT; y++) {
-        for(int x=0; x<BOARD_WIDTH; x++)
-            if((x==cursorX) && (y==cursorY))
-                printf("◎");
-            else    
-                printf("・");
-        printf("\n");
-    }
-   
+    while(1){
+        system("c|s");
+        for(int y=0; y<BOARD_HEIGHT; y++) {
+            for(int x=0; x<BOARD_WIDTH; x++)
+                if((x==cursorX) && (y==cursorY))
+                    printf("◎");
+                else{
+                    switch(cells[y][x]){
+                    case COLOR_NOME:    printf("・");    break;
+                    case COLOR_BLACK:   printf("⚫︎");    break;
+                    case COLOR_WHITE:   printf("⚪︎");    break;
+                    }
+                }    
+                    
+            printf("\n");
+        }
 
+        switch(getchar()){
+        case'w': cursorY--; break;  //上へ
+        case's': cursorY++; break;  //下へ
+        case'a': cursorX--; break;  //左へ
+        case'd': cursorX++; break;  //右へ
+        default:
+            break;
+        }
+    }
 }
